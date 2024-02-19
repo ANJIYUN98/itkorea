809ec8964f088dc331ee9711d4e9b8d28aa3cf05
ANJIYUN98 <155033351+ANJIYUN98@users.noreply.github.com>
2024-02-19 13:18:59 +0900

    Add files via upload

---
+++ 15_함수/js/script01.js
@@ -0,0 +1,6 @@
+function f1(){
+    console.log('script01_f1 Function call!');
+}
+const f1_01=()=>{
+    console.log('script01_f1_01 Function call!');
+} //지역화 시킴, const로 지역화 시키면 함수명이 동일했을때 오류를 잡아 낼 수 있음
---
+++ 15_함수/js/script02.js
@@ -0,0 +1,6 @@
+function f1(){
+    console.log('script02_f1 Function call!');
+}
+const f1_01=()=>{
+    console.log('script02_f1_01 Function call!');
+} //지역화 시킴
